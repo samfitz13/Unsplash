40365e7eee58425cc2969f1d34fe8c836babfec9
Sam Fitz <sam@samfitz.me>
2021-05-18 19:41:23 -0500

    installed react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import * as Realm from "realm-web";
+
+const REALM_APP_ID = "unsplash-wrfxf";
+const app = new Realm.App({ id: REALM_APP_ID });
+
+const PostSchema = {
+	name: "Post",
+	properties: {
+		_id: "objectId",
+		url: "string",
+		label: "string",
+	},
+};
+
+const run = async () => {
+	await app.logIn(new Realm.Credentials.anonymous());
+
+	const realm = await Realm.open({
+		schema: [PostSchema],
+		sync: {
+			user: app.currentUser,
+			partitionValue: "myPartition",
+		},
+	});
+};
+
+const UserDetail = ({ user }) => {
+	return (
+		<div>
+			<h1>Logged in with anonymous id: {user.id}</h1>
+		</div>
+	);
+};
+
+// Create a component that lets an anonymous user log in
+const Login = ({ setUser }) => {
+	const loginAnonymous = async () => {
+		const user = await app.logIn(Realm.Credentials.anonymous());
+		setUser(user);
+	};
+	return <button onClick={loginAnonymous}>Log In</button>;
+};
+
+const App = () => {
+	// Keep the logged in Realm user in local state.
+	// whenever the current user changes (e.g. logs in or logs out).
+	const [user, setUser] = React.useState(app.currentUser);
+
+	// If a user is logged in, show their details.
+	// Otherwise, show the login screen.
+	return (
+		<div className="App">
+			<div className="App-header">
+				{user ? <UserDetail user={user} /> : <Login setUser={setUser} />}
+			</div>
+		</div>
+	);
+};
 
 export default App;
